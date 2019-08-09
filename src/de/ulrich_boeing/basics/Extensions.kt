32353aa08c772da5f443cf6793eac6c170954cff
Ulrich <songs.of.letting.go@gmail.com>
2019-08-09 11:09:28 +0200

    Extensions for comparing images

--- src/de/ulrich_boeing/basics/Extensions.kt
+++ src/de/ulrich_boeing/basics/Extensions.kt
@@ -1,5 +1,8 @@
 package de.ulrich_boeing.basics
 
+import processing.core.PGraphics
+import processing.core.PImage
+import kotlin.math.absoluteValue
 import kotlin.random.Random
 
 fun Random.floatInRange(from: Float, until: Float) = from + this.nextFloat() * (until - from)
@@ -12,4 +15,99 @@ fun Random.intInRange(from: Int, until: Int): Int {
         from < until -> nextInt(from, until)
         else -> nextInt(until, from)
     }
+}
+
+fun PImage.toIntArray(): IntArray {
+    val arr = IntArray(this.pixels.size * 3)
+
+    this.loadPixels()
+    for (i in this.pixels.indices) {
+        val color = Color(pixels[i])
+        arr[i * 3] = color.red
+        arr[i * 3 + 1] = color.green
+        arr[i * 3 + 2] = color.blue
+    }
+    this.updatePixels()
+
+    return arr
+}
+
+fun PImage.toByteArray(): ByteArray {
+    val arr = ByteArray(this.pixels.size * 3)
+
+    this.loadPixels()
+    for (i in this.pixels.indices) {
+        val color = Color(pixels[i])
+        arr[i * 3] = color.red.toByte()
+        arr[i * 3 + 1] = color.green.toByte()
+        arr[i * 3 + 2] = color.blue.toByte()
+    }
+    this.updatePixels()
+
+    return arr
+}
+
+fun PImage.getDifference(g: PGraphics, xOffset: Int, yOffset: Int): Long {
+    var sum: Long = 0
+
+    g.loadPixels()
+    for (y in 0 until g.height) {
+        for (x in 0 until g.width) {
+            val i1 = (x + xOffset) + (y + yOffset) * this.width
+            val i2 = x + y * g.width
+
+            val r1 = (this.pixels[i1] shr 16) and 0xFF
+            val g1 = (this.pixels[i1] shr 8) and 0xFF
+            val b1 = this.pixels[i1] and 0xFF
+
+            val r2 = (g.pixels[i2] shr 16) and 0xFF
+            val g2 = (g.pixels[i2] shr 8) and 0xFF
+            val b2 = g.pixels[i2] and 0xFF
+
+            sum += (r1 - r2).absoluteValue + (g1 - g2).absoluteValue + (b1 - b2).absoluteValue
+        }
+    }
+    return sum
+}
+
+fun red(color: Int): Int = (color shr 16) and 0xFF
+fun green(color: Int): Int = (color shr 8) and 0xFF
+fun blue(color: Int): Int = color and 0xFF
+
+
+fun PImage.sumRGBRect(rect: IntRect): Int {
+    var sum: Int = 0
+    for (x in rect.x until rect.x + rect.width) {
+        for (y in rect.y until rect.y + rect.height) {
+            val i = x + y * this.width
+            val r = pixels[i] shr 16 and 0xFF  // Faster way of getting red(argb)
+            val g = pixels[i] shr 8 and 0xFF   // Faster way of getting green(argb)
+            val b = pixels[i] and 0xFF          // Faster way of getting blue(argb)
+            sum += r + rgbDifference(g, b)
+
+        }
+    }
+    return sum
+}
+
+fun PImage.sumRGB(): Int {
+    var sum: Int = 0
+
+//    this.loadPixels()
+    for (i in this.pixels.indices) {
+//        val color = Color(pixels[i])
+//        sum +=  color.red + color.green + color.blue
+
+        val r = pixels[i] shr 16 and 0xFF  // Faster way of getting red(argb)
+        val g = pixels[i] shr 8 and 0xFF   // Faster way of getting green(argb)
+        val b = pixels[i] and 0xFF          // Faster way of getting blue(argb)
+        sum += r + rgbDifference(g, b)
+    }
+//    this.updatePixels()
+
+    return sum
+}
+
+inline fun rgbDifference(color1: Int, color2: Int): Int {
+    return color1 + color2
 }
--- src/de/ulrich_boeing/elements/Layout.kt
+++ src/de/ulrich_boeing/elements/Layout.kt
@@ -1,41 +1,80 @@
 package de.ulrich_boeing.elements
 
 import de.ulrich_boeing.basics.Color
+import de.ulrich_boeing.basics.Rect
+import de.ulrich_boeing.basics.Vec
 import processing.core.PApplet
 import processing.core.PGraphics
 import processing.core.PImage
+import java.lang.RuntimeException
 import kotlin.random.Random
 
 class Layout(val app: PApplet) {
-    var g : PGraphics = app.createGraphics(app.width, app.height)
-    val list = mutableListOf<Element>()
+    private val elements = mutableListOf<Element>()
+    var g: PGraphics? = null
+
+    var fitness: Double = -1.0
+        get() {
+            if (field == -1.0)
+                throw RuntimeException("Fitness is not calculated yet. Please render() before.")
+
+            return field
+        }
+        private set(value) {
+            field = value
+        }
+
 
     fun add(e: Element) {
-        list.add(e)
+        if (g != null)
+            throw RuntimeException("Can't add to Layout which has been rendered.")
+
+        elements.add(e)
     }
 
     fun mutate(): Layout {
         val newLayout = Layout(app)
-        newLayout.list.addAll(list)
+        newLayout.elements.addAll(elements)
 
         for (i in 1..1) {
-            val i = Random.nextInt(newLayout.list.size)
-            newLayout.list[i] = newLayout.list[i].mutate()
+            val i = Random.nextInt(newLayout.elements.size)
+            newLayout.elements[i] = newLayout.elements[i].mutate()
         }
         return newLayout
     }
 
-    fun draw() {
+    fun render(image: PImage) {
+        g = app.createGraphics(image.width, image.height)
+        if (g != null) {
+            draw(g!!)
+            fitness = compareImage(g!!, image)
+        }
+    }
+
+    fun renderElement(i: Int): PGraphics {
+        val rect = elements[i].rect.toIntRect()
+        val offset = Vec(rect.x, rect.y)
+
+        val gElement = app.createGraphics(rect.width, rect.height)
+        gElement.beginDraw()
+        gElement.background(0)
+        for (e in elements) {
+            e.drawWithOffset(gElement, offset)
+        }
+        gElement.endDraw()
+        return gElement
+    }
+
+    private fun draw(g: PGraphics) {
         g.beginDraw()
-        g.background(255)
-        for (e in list) {
+        g.background(0)
+        for (e in elements) {
             e.draw(g)
         }
         g.endDraw()
     }
 
-    fun fitness(image: PImage): Double {
-        draw()
+    private fun compareImage(g: PGraphics, image: PImage): Double {
         g.loadPixels()
         image.loadPixels()
 
@@ -50,6 +89,6 @@ class Layout(val app: PApplet) {
         image.updatePixels()
 
         // current: the smaller the fitness, the better
-        return sum / (image.width * image.height)
+        return sum / (image.pixels.size)
     }
 }
