270aafac5853ff151723cda883a8ea9a3730d095
Ulrich <songs.of.letting.go@gmail.com>
2019-09-10 13:02:50 +0200

    Putting Population into Adaptable

--- src/de/ulrich_boeing/adaptable/Adaptable.kt
+++ src/de/ulrich_boeing/adaptable/Adaptable.kt
@@ -1,69 +1,105 @@
 package de.ulrich_boeing.adaptable
 
-import de.ulrich_boeing.basics.Clipping
-import de.ulrich_boeing.basics.Color
-import de.ulrich_boeing.basics.Vec
-import de.ulrich_boeing.basics.getDifference
+import com.sun.javafx.scene.control.skin.VirtualFlow
+import de.ulrich_boeing.basics.*
 import de.ulrich_boeing.fitness_tools.FloatRange
 import processing.core.PApplet
 import processing.core.PGraphics
 import processing.core.PImage
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.random.Random
 
 typealias DNA = FloatArray
+typealias Structure = Array<Vec>
+
+class PopulationStats(val bestFitness: Float, val worstFitness: Float)
+
+class AdaptableStats() {
+    val populationStats = ArrayList<PopulationStats>(200)
+    val history: Queue<PopulationStats> = ArrayDeque<PopulationStats>()
+
+    fun test() {
+        history.add(PopulationStats(0f, 0f))
+        if (history.size == 10)
+            history.remove()
+        for (item in history) {
+            println(item.bestFitness)
+            history.remove(item)
+        }
+    }
 
 
+    var generations: Int = 0
+    var bestFitness = -1f
+}
 
-
-class Adaptable(val app: PApplet, aDNA: Array<Float>) {
-    constructor (app: PApplet, pointsCount: Int = 6): this(app, Array(pointsCount + 1) { Random.nextFloat() })
-
-    val pointsCount = aDNA.size - 1
-    val radiusRange = FloatRange(0.25f, 0.95f)
+class Adaptable(val app: PApplet, val clipping: Clipping, val populationCount: Int = 1) {
+    /*
+        Fixed parameter of Adaptable
+    */
+    val pointsCount = 6
+    val radiusRange = FloatRange(0.1f, 0.95f)
     val color = Color.fromRGBA(0, 0, 255, 180)
-    val DNA = aDNA
 
-    fun mutate(rate: Float) {
-        for (i in DNA.indices) {
-            if (Random.nextFloat() < rate)
-                DNA[i] = Random.nextFloat()
+    /*
+        parameter of the genetic algorithm
+    */
+    private val sizeDNA = 7
+    var population = initPopulation()
+    var mutationRate = 0.06f
+    var mutationRange = 0.02f
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    val stats = AdaptableStats()
+
+
+    // Cache Variables
+    val angleStepSize = Vec.TAU / pointsCount
+    var allNormStructures = createAllNormStructures()
+    var fitness = FloatArray(populationCount)
+
+    fun createAllNormStructures(): Array<Structure> = Array(populationCount) { i -> createNormStructure(population[i]) }
+
+    fun createNormStructure(dna: DNA): Structure {
+        val start = angleStepSize * (dna.last() - 0.5f)
+        val angles = FloatArray(pointsCount) { i ->
+            start + i * angleStepSize
         }
-    }
-
-    fun createPoints(clipping: Clipping): Array<Vec> {
-        val range = Vec.TAU / pointsCount
-        val start = range * (DNA.last() - 0.5f)
-        val angles = Array(pointsCount) { i ->
-            start + i * range
+        val lengths = FloatArray(pointsCount) { i ->
+            radiusRange.expand(dna[i])
         }
-
-        val r1 = clipping.width / 2f
-        val r2 = clipping.height / 2f
-
-        val lengths = Array<Float>(pointsCount) { i -> radiusRange.expand(DNA[i]) }
         return Array(pointsCount) { i ->
-            Vec(r1 * lengths[i] * cos(angles[i]), r2 * lengths[i] * sin(angles[i]))
+            Vec(lengths[i] * cos(angles[i]), lengths[i] * sin(angles[i]))
         }
     }
 
+    fun expandAllStructures(scale: Float): Array<Structure> =
+        Array<Structure>(populationCount) { i -> expandStructure(allNormStructures[i], scale) }
 
+    fun expandStructure(normStructure: Structure, scale: Float): Structure {
+        val r1 = (clipping.width * scale) / 2f
+        val r2 = (clipping.height * scale) / 2f
+        return Array(pointsCount) { i ->
+            Vec(normStructure[i].x * r1, normStructure[i].y * r2)
+        }
+    }
 
-    fun getImage(clipping: Clipping): PGraphics {
-        val points = createPoints(clipping)
-
-        val g = app.createGraphics(clipping.width, clipping.height)
+    fun getImage(structure: Structure, targetClipping: Clipping): PGraphics {
+        val width = targetClipping.width
+        val height = targetClipping.height
+        val g = app.createGraphics(width, height)
         g.beginDraw()
         g.background(0)
         g.noStroke()
 
-        g.translate(clipping.width / 2f, clipping.height / 2f)
+        g.translate(width / 2f, height / 2f)
         g.fill(color.rgba)
         g.beginShape()
 //        curveVertex(center.x + vecs.last().x, center.y + vecs.last().y)
         for (i in 1..2)
-            for (point in points) {
+            for (point in structure) {
                 g.curveVertex(point.x, point.y)
             }
 //        curveVertex(center.x + vecs.first().x, center.y + vecs.first().y)
@@ -73,9 +109,60 @@ class Adaptable(val app: PApplet, aDNA: Array<Float>) {
         return g
     }
 
-    fun fitness(image: PImage, graphic: PGraphics): Float {
-        val dif = image.getDifference(graphic)
-        val colorValuesCount = graphic.width * graphic.height * 3 * 255
-        return 1 - (dif / colorValuesCount.toFloat())
+    fun getImage(scale: Float, index: Int = 0): PGraphics {
+        val structure = expandStructure(allNormStructures[index], scale)
+        return getImage(structure, clipping)
+    }
+
+    fun evaluateFitness(target: PImage, targetClipping: Clipping) {
+        val scale = targetClipping.width / clipping.width.toFloat()
+        val allStructures = expandAllStructures(scale)
+
+        for (i in allStructures.indices) {
+            val image = getImage(allStructures[i], targetClipping)
+            val dif = target.getDifference(image)
+            val colorValuesCount = image.width * image.height * 3 * 255
+            fitness[i] = 1 - (dif / colorValuesCount.toFloat())
+
+        }
+//        val sortedFitness = fitness.sortedArray()
+    }
+
+    fun getBestFitness(): Int = fitness.indexOfMax()
+
+    fun mutate(dna: DNA) {
+        for (i in dna.indices) {
+            if (Random.nextFloat() < mutationRate) {
+                val new = Random.nextFloat()
+                dna[i] = (1 - mutationRange) * dna[i] + mutationRange * new
+            }
+        }
+    }
+
+    private fun initDNA(): DNA = FloatArray(sizeDNA) { Random.nextFloat() }
+
+    private fun initPopulation(): Array<DNA> = Array(populationCount) { initDNA() }
+
+    private fun createDNA(): DNA {
+        val i1 = fitness.selectOne()
+        val i2 = fitness.selectOne()
+        val newDNA = population[i1].mix(population[i2], Random.nextInt(sizeDNA - 1))
+        mutate(newDNA)
+        return newDNA
+    }
+
+    private fun createPopulation(): Array<DNA> = Array(populationCount) { createDNA() }
+
+    private fun evaluatePopulation() {
+        stats.populationStats.add(PopulationStats(fitness.max() ?: 0f, fitness.min() ?: 0f))
+    }
+
+    fun nextGeneration(target: PImage, targetClipping: Clipping) {
+        evaluateFitness(target, targetClipping)
+        evaluatePopulation()
+        fitness.norm()
+        population = createPopulation()
+        stats.generations++
+        allNormStructures = createAllNormStructures()
     }
 }
--- src/de/ulrich_boeing/adaptable/Population.kt
+++ src/de/ulrich_boeing/adaptable/Population.kt
@@ -1,35 +1,35 @@
 package de.ulrich_boeing.adaptable
 
-import de.ulrich_boeing.basics.Clipping
-import de.ulrich_boeing.basics.mix
-import processing.core.PApplet
-import processing.core.PImage
-
-class Population(val app: PApplet, val target: PImage, val count: Int) {
-    var population = Array<Adaptable>(count) { Adaptable(app) }
-    var generations = 0
-
-    fun fitness(clipping: Clipping): Array<Float> {
-        return Array(population.size) { i ->
-            population[i].fitness(target, population[i].getImage(clipping))
-        }
-    }
-
-    fun list(fitness: Array<Float>): Array<Pair<Adaptable, Float>> {
-        return Array(population.size) { i ->
-            Pair(population[i], fitness[i])
-        }
-    }
-
-    fun creation(male: Adaptable, female: Adaptable): Adaptable {
-        return Adaptable(male.app, male.DNA.mix(female.DNA, 3))
-    }
-
-    fun nextGeneration() {
-
-    }
-
-
-
-
-}
+//import de.ulrich_boeing.basics.Clipping
+//import de.ulrich_boeing.basics.mix
+//import processing.core.PApplet
+//import processing.core.PImage
+//
+//class Population(val app: PApplet, val target: PImage, val count: Int) {
+//    var population = Array<Adaptable>(count) { Adaptable(app) }
+//    var generations = 0
+//
+//    fun fitness(clipping: Clipping): Array<Float> {
+//        return Array(population.size) { i ->
+//            population[i].fitness(target, population[i].getImage(clipping))
+//        }
+//    }
+//
+//    fun list(fitness: Array<Float>): Array<Pair<Adaptable, Float>> {
+//        return Array(population.size) { i ->
+//            Pair(population[i], fitness[i])
+//        }
+//    }
+//
+//    fun creation(male: Adaptable, female: Adaptable): Adaptable {
+//        return Adaptable(male.app, male.DNAs.mix(female.DNAs, 3))
+//    }
+//
+//    fun nextGeneration() {
+//
+//    }
+//
+//
+//
+//
+//}
